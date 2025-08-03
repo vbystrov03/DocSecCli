6bf0f1f6e33d8d6a0a57b5a9e6766768daddfbad
Vladimir Bystrov <vladimir.bystrov@galaktika.ru>
2025-08-03 13:46:04 +0300

    first commit

---
+++ DocSecCli/cli/test.py
@@ -0,0 +1,47 @@
+import fire, subprocess, yaml
+
+class ScanDocker():
+
+    #Функция чтения политики из файла
+    def ReadPolicyFile(PolicyFileName="policy.yml"):
+        with open(PolicyFileName, 'r') as PFile:
+            policy = yaml.safe_load(PFile) or {}
+        print(policy)
+        
+        policy_dict = policy.get("Policy")  # может быть None
+        
+        if isinstance(policy_dict, dict) and "LevelUp" in policy_dict:
+            return policy_dict["LevelUp"]
+        else:
+            return ['HIGH', 'CRITICAL']
+        
+    # Функция для анализа вывода
+    def AnalysisOut(result):
+        levels = ScanDocker.ReadPolicyFile()
+        found_lines = [
+            line for line in result.splitlines()
+            if any(level in line for level in levels)
+        ]
+        if found_lines:
+            return "\n".join(found_lines)
+        else:
+            return "Уязвимостей нет"
+        
+    # Функция для сканирования образов Docker
+    def scan(self, ImageID):
+        Args = []
+        Args.append("trivy")
+        Args.append("image")
+        Args.append(ImageID)
+        result = subprocess.run(
+            Args,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.STDOUT,
+            universal_newlines = True
+        )
+        filtered_output = ScanDocker.AnalysisOut(result.stdout)
+        print(filtered_output)
+
+if __name__ == "__main__":
+    obj = ScanDocker()
+    fire.Fire(obj)
